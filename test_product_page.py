5a86ffaa4047e287079e1c97de758372e09042e0
zigol <zigolenko@rambler.ru>
2023-12-10 22:55:05 +0300

    Задание: независимость от данных

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,7 @@ import time
 
 @pytest.mark.xfail(reason="wrong message")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
