47938f4720da605ece12f22016291eb5363138d1
zigol <zigolenko@rambler.ru>
2023-12-10 21:42:33 +0300

    Begin 4.3

--- locators.py
+++ locators.py
@@ -1,5 +1,10 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators ():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    USER_EMAIL =(By.CSS_SELECTOR,  '#id_registration-email')
+    USER_PASS1 =(By.CSS_SELECTOR,  '#id_registration-password1')
+    USER_PASS2 =(By.CSS_SELECTOR,  '#id_registration-password2')
+    BUTTON_REGISTER_USER =(By.XPATH,  '//button [@name="registration_submit"]')
