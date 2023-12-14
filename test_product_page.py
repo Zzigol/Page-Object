b4709bd90df65ac3d1c5e33db7d1f06284004696
zigol <zigolenko@rambler.ru>
2023-12-14 23:46:16 +0300

    Ждут 2 и 3, проходит 2, падает 1 и 3

--- test_product_page.py
+++ test_product_page.py
@@ -4,18 +4,18 @@ from pages.login_page import LoginPage
 import pytest
 import random
 import time
-
+'''
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail)])
                                   # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
-
+@pytest.mark.skip
 @pytest.mark.need_review
 
 def test_guest_can_add_product_to_basket(browser, link):
@@ -26,22 +26,37 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.solve_quiz_and_get_code()
     product_page.should_be_book_name()
     product_page.should_be_book_price()
+    '''
 
 
 
 
-
-@pytest.mark.skip
-#@pytest.mark.xfail(reason="wrong message")
+#@pytest.mark.skip
+@pytest.mark.xfail(reason="wrong message")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
-    product_page.solve_quiz_and_get_code()
-    time.sleep(120)
+    #product_page.solve_quiz_and_get_code()
+    #time.sleep(120)
     product_page.should_not_be_success_message()
-    product_page.should_be_book_price()
     
 #solve_quiz_and_get_code python -m pytest -v --tb=line --language=en test_product_page.py
 # python -m pytest -s test_product_page.py
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason="not disappiring")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.is_dissappeared()
